4adfcc7dbbfc984a91f9aac181fe124364bad58e
seanbrittian <39929810+seanbrittian@users.noreply.github.com>
2021-07-19 09:05:59 -0400

    Add

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\sean.brittian\source\repos\ThinLineWeapons\ThinLineWeapons\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
